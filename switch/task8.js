d4be1ae403c0ea8ba3df8676f6dd6a926430d73a
Svemiliva <mshtatkich@gmail.com>
2021-03-12 18:16:46 +0100

    Switch excercises7

---
+++ switch/task8.js
@@ -0,0 +1,23 @@
+/*Write a program that takes as input two numbers and a string denoting the operation (“+”, “-
+    ”, “*”, “/”) and prints out the appropriate result. Watch out for division by zero!*/
+
+var a = 2
+var b = 3
+var result
+switch (result) {
+    case a + b:
+        console.log(result)
+
+        break;
+    case a - b:
+        console.log(result)
+        break;
+    case a * b:
+        console.log(result)
+        break;
+    case a / b:
+        console.log(result)
+
+    default:
+        break;
+}
