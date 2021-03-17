ed9cabde4cd4d5f5d5c3667260227c2fa2519cf1
Svemiliva <mshtatkich@gmail.com>
2021-03-17 12:28:40 +0100

    Arrays and Loops

---
+++ Arrays/task4.js
@@ -0,0 +1,9 @@
+/*Print all elements with indices divisible by 3 from the array [5, 15, -5, 20, 12, 18,
+    72, 14, 9].*/
+
+
+    var array= [5, 15, -5, 20, 12, 18, 72, 14, 9]
+    console.log (array [0])
+    console.log (array [3])
+    console.log (array [6])
+ 
